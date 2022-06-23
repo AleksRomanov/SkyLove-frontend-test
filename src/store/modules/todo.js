e7fd1321de82a58ce62d040179a8ecbf83705789
aleksromanov <kosmonavt-vavilon@mail.ru>
2022-06-23 23:37:43 +0700

    work with VUEX. Переход на локальные данные

--- src/store/modules/todo.js
+++ src/store/modules/todo.js
@@ -2,7 +2,8 @@ export default {
     actions: {
         async fetchTodos(ctx) {
             // const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=" + limit)
-            const res = await fetch("https://api.jsonbin.io/b/62b4887a402a5b3802358676/1")
+            // const res = await fetch("https://api.jsonbin.io/b/62b4887a402a5b3802358676/2")
+            const res = await fetch("https://drive.google.com/file/d/1vxRTexZanygHXm2CVvowHdf6-cBaAJln/view?usp=sharing")
             const todos = await res.json()
             ctx.commit('updateTodos', todos)
         }
@@ -12,6 +13,9 @@ export default {
             state.todos = todos
         },
         createTodo(state, newTodo) {
+            // console.log("!!!newTodo!!!")
+            // console.log(state)
+            // console.log(newTodo)
             state.todos.unshift(newTodo)
         }
     },
